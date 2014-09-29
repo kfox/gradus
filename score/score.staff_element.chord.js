a13432050475912dd109b2982b3db2de330e2110
Peter Woo <petersnowdonwoo@gmail.com>
2014-09-29 12:44:00 -0700

    Rename some files

--- score/score.staff_element.chord.js
+++
@@ -1,7 +0,0 @@
-Score.ChordBegin = function(){}
-Score.ChordBegin.prototype = new Score.StaffElement();
-Score.ChordBegin.prototype.type = 'ChordBegin';
-
-Score.ChordEnd = function(){}
-Score.ChordEnd.prototype = new Score.StaffElement();
-Score.ChordEnd.prototype.type = 'ChordEnd';
