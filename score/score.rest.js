193c275d70d4f30c8d90f12b88fab0331bcb3f31
Peter Woo <petersnowdonwoo@gmail.com>
2014-09-30 13:02:17 -0700

    Fix #1 (whole/half rest positions)

--- score/score.rest.js
+++ score/score.rest.js
@@ -9,7 +9,17 @@ Score.Rest.prototype.glyphValue = Score.Note.prototype.glyphValue;
 Score.Rest.prototype.render = function(svg, x, y) {
   var rest = Glyph.Rest.render(svg, this.glyphValue());
   var box = rest.bbox();
-  rest.move(x + box.width/2, y + 2*Score.Staff.LINE_HEIGHT - box.height/2);
+  switch(this.glyphValue()) {
+  case 'whole':
+    rest.move(x + box.width/2, y + Score.Staff.LINE_HEIGHT);
+    break;
+  case 'half':
+    rest.move(x + box.width/2, y + 2*Score.Staff.LINE_HEIGHT - box.height);
+    break;
+  default:
+    rest.move(x + box.width/2, y + 2*Score.Staff.LINE_HEIGHT - box.height/2);
+  }
+
   this.avatar = rest;
   this.bindListeners();
 };
